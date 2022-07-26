ae0d36a99dca3ed3abd4a0969ea2edd548186a5f
BarburKoeroglu <b.koeroglu@proton.me>
2022-07-26 10:35:22 +0200

    add useToDo hook

---
+++ src/useToDo.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from "axios";
+import {ToDoItem} from "./ToDoItem";
+import ShowList from "./ShowList";
+import {ItemStatus} from "./ItemStatus";
+import PostToDo, {PostToDoProps} from "./PostToDo";
+import { getNextStatus } from './todo-service';
+import useToDo from './useToDo';
+
+
+export default function useToDo() {
+const [toDos, setToDos] = useState<ToDoItem[]>([])
+
+useEffect(() => {
+    getToDos()
+},[])
+
+const getToDos = () => {
+    axios.get("/api/todo")
+        .then((response) => response.data)
+        .then(data => setToDos(data))
+        .catch((error) => console.error(error))             // Wann wird catch notwendig?
+}
+
+const postToDos = (description: string) => {
+    const newItem = {
+        description: description,
+        status: ItemStatus.OPEN                         // Status OPEN wird vordefiniert
+    }
+
+    axios.post("/api/todo", newItem)
+        .then(getToDos)
+}
+
+
+const advanceToDo = (todo: ToDoItem) => {
+    const updateToDo = {
+        id: toDos.id,
+        description: toDos.description,
+        status: getNextStatus(toDos.status)
+    }
+
+    axios.put(`/api/todo/${todo.id}`, updateToDo)
+        .then(getToDos())
+}
+}
+
+const deleteToDo=(id:string) => {
+    axios.delete(`/api/todo/${id}`)
+        .then(getToDos)
+
+}
